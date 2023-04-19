4eb417d7694592ab1145fd4ebf1bbf3e74ce967d
lztech-tp <62520358+lztech-tp@users.noreply.github.com>
2023-04-19 09:56:45 -0400

    updated app

--- app.py
+++ app.py
@@ -3,32 +3,6 @@ from ggsearch import google_search
 from extract_prices import extract_prices
 
 
-""" def main():
-    st.title("Price Scraper")
-    st.write("Enter a product name to search and scrape prices from Google search results:")
-
-    query = st.text_input("Product name")
-    num_results = st.number_input("Number of search results", min_value=1, max_value=50, value=10, step=1)
-
-    if st.button("Search and Scrape"):
-        urls = google_search(query, num_results=num_results)
-        product_prices = []
-
-        with st.spinner("Scraping prices..."):
-            for url in urls:
-                try:
-                    prices = extract_prices(url)
-                    if prices:
-                        product_prices.extend(prices)
-                except:
-                    continue
-
-        st.write("Prices found:")
-        for price in product_prices:
-            st.write(price)
-
-if __name__ == '__main__':
-    main() """
 
 def main():
     st.title("Product Price Scraper")
@@ -51,7 +25,10 @@ def main():
 
         st.markdown("### Prices found:")
         for price in product_prices:
-            st.write(f"{price[1]} - [Source]({price[0]})")
+            st.write(f"{price[2]} - {price[0]} - [Source]({price[1]})")
 
 if __name__ == "__main__":
     main()
+
+
+
--- extract_prices.py
+++ extract_prices.py
@@ -2,16 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+
+
 def extract_prices(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.content, 'html.parser')
 
+    site_title = soup.title.string if soup.title else 'Unknown'
+
     price_regex = re.compile(r'(\$\d+(?:\.\d{2})?)')
     prices = []
 
     for match in price_regex.finditer(soup.text):
-        prices.append(match.group(1))
+        prices.append((site_title, url, match.group(1)))
 
     return prices
+
