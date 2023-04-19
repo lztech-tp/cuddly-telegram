59c42760e732ac905fb75e2e98195cf98dc28d14
lztech-tp <62520358+lztech-tp@users.noreply.github.com>
2023-04-19 09:48:20 -0400

    updated app code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from ggsearch import google_search
 from extract_prices import extract_prices
 
 
-def main():
+""" def main():
     st.title("Price Scraper")
     st.write("Enter a product name to search and scrape prices from Google search results:")
 
@@ -28,4 +28,30 @@ def main():
             st.write(price)
 
 if __name__ == '__main__':
+    main() """
+
+def main():
+    st.title("Product Price Scraper")
+    query = st.text_input("Enter a product name to search:", "Samsung French Door Refrigerator")
+    num_results = st.slider("Number of search results:", min_value=2, max_value=50, value=10, step=1)
+
+    if st.button("Search"):
+        urls = google_search(query, num_results=num_results)
+        product_prices = []
+
+        with st.spinner("Scraping prices..."):
+
+            for url in urls:
+                try:
+                    prices = extract_prices(url)
+                    if prices:
+                        product_prices.extend(prices)
+                except:
+                    continue
+
+        st.markdown("### Prices found:")
+        for price in product_prices:
+            st.write(f"{price[1]} - [Source]({price[0]})")
+
+if __name__ == "__main__":
     main()
